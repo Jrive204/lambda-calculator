2917e3108abaeefa18bd771883e18dfd7506b1d4
Jose <Jrivera6869@yahoo.com>
2019-12-10 16:08:12 -0500

    fixing up numbers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState }from "react";
 import "./App.css";
+import Numbers from './components/ButtonComponents/NumberButtons/Numbers'
+import { numbers, operator, specials } from './data';
+
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
 
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
+
+// const [numberState, setNumberState] = useState(numbers);
+
 
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
@@ -16,6 +22,7 @@ function App() {
   return (
     <div className="container">
       <Logo />
+      
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
       </div>
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {numbers} from '../../../data'
 
 //import any components needed
 // example of import from data.js. Note all the ../   This is how we move through folders. 
@@ -9,11 +10,17 @@ import { numbers } from '../../../data'
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
+  let numbuttons = numbers.map((number) => 
+    <button>{number}</button>
+     );
+  
   return (
-    <div>
+    <div>      
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {numbuttons}
     </div>
   );
 };
+export default Numbers
